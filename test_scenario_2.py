d11504a5dcc74c9b315c8cfca2f55004f2a520a0
parsa-yazdan <parsa.yazdankhah@gmail.com>
2023-08-28 13:29:02 +0330

    improved performance

--- test_scenario_2.py
+++ test_scenario_2.py
@@ -32,6 +32,7 @@ def colision_detector(ego_vehicle: carla.Vehicle, second_vehicle: carla.Vehicle)
     second_transform = second_vehicle.get_transform()
     second_location = np.array([second_transform.location.x, second_transform.location.y, second_transform.location.z])
     relative_distance = second_location - ego_location
+    distance = ego_vehicle.get_location().distance(second_vehicle.get_location())
 
     refernce_unit_vector = np.array([ego_transform.get_forward_vector().x, ego_transform.get_forward_vector().y, ego_transform.get_forward_vector().z])
 
@@ -40,58 +41,76 @@ def colision_detector(ego_vehicle: carla.Vehicle, second_vehicle: carla.Vehicle)
 
     t = np.linalg.norm(ego_velocity_vector) / 9.81
 
-    if relative_velocity_local < 0 and (relative_distance_local-3)/abs(relative_velocity_local) < t:
+    distance = distance - max(ego_vehicle.bounding_box.extent.y, ego_vehicle.bounding_box.extent.x) \
+                        - max(second_vehicle.bounding_box.extent.y, second_vehicle.bounding_box.extent.x)
+
+    if relative_velocity_local < 0 and (distance)/abs(relative_velocity_local) < t:
+        print(f'time to collision: {t:.4f} sec')
         return True
 
 
 def main():
 
-    client = carla.Client('localhost', 2000)
-    client.set_timeout(2.0)
-    world = client.get_world()
+    try:
+        client = carla.Client('localhost', 2000)
+        client.set_timeout(2.0)
+        world = client.get_world()
 
-    # Create a synchronous mode context
-    settings = world.get_settings()
-    settings.synchronous_mode = True
-    settings.fixed_delta_seconds = 0.1  # Simulation step size in seconds
-    world.apply_settings(settings)
+        # Create a synchronous mode context
+        settings = world.get_settings()
+        settings.synchronous_mode = True
+        settings.fixed_delta_seconds = 0.1  # Simulation step size in seconds
+        world.apply_settings(settings)
 
-    ego_vehicle = None
-    while ego_vehicle is None:
-        print("Waiting for the ego vehicle...")
-        time.sleep(1)
-        possible_vehicles = world.get_actors().filter('vehicle.*')
-        for vehicle in possible_vehicles:
-            if vehicle.attributes['role_name'] == 'hero':
-                print("Ego vehicle found")
-                ego_vehicle = vehicle
-                break
-    
-    bp = random.choice(world.get_blueprint_library().filter('vehicle.dodge.charger_2020'))
-    transform = ego_vehicle.get_transform()
-    transform.location.x += 100
-    second_vehicle = world.try_spawn_actor(bp, transform)
-
-    spectator = world.get_spectator()
-    second_vehicle_control = second_vehicle.get_control()
-    ego_vehicle_control = ego_vehicle.get_control()
-
-    ego_vehicle_control.throttle = 0.3
-    ego_vehicle.apply_control(ego_vehicle_control)
-
-    second_vehicle_control.throttle = 0.3
-    second_vehicle.apply_control(second_vehicle_control)
-
-    while True:
+        ego_vehicle = None
+        while ego_vehicle is None:
+            print("Waiting for the ego vehicle...")
+            time.sleep(1)
+            possible_vehicles = world.get_actors().filter('vehicle.*')
+            for vehicle in possible_vehicles:
+                if vehicle.attributes['role_name'] == 'hero':
+                    print("Ego vehicle found")
+                    ego_vehicle = vehicle
+                    break
+        
+        bp = random.choice(world.get_blueprint_library().filter('vehicle.dodge.charger_2020'))
         transform = ego_vehicle.get_transform()
-        spectator.set_transform(carla.Transform(transform.location + carla.Location(z=30),carla.Rotation(pitch=-90)))
+        transform.location.x += 70
+        transform.location.z += 1
+        second_vehicle = world.try_spawn_actor(bp, transform)
 
-        if colision_detector(ego_vehicle, second_vehicle):
-            ego_vehicle_control.throttle = 0.0
-            ego_vehicle_control.brake = 1.0
-            ego_vehicle.apply_control(ego_vehicle_control)
-            print("Collision is about to occur !!!!!!")
+        spectator = world.get_spectator()
+        # second_vehicle_control = second_vehicle.get_control()
+        ego_vehicle_control = ego_vehicle.get_control()
 
+        ego_vehicle_control.throttle = 1.0
+        ego_vehicle_control.brake = 0.0
+        ego_vehicle_control.steer = 0.0
+        ego_vehicle.apply_control(ego_vehicle_control)
+
+        # second_vehicle_control.throttle = 0.3
+        # second_vehicle_control.brake = 0.0
+        # second_vehicle_control.steer = 0.0
+        # second_vehicle.apply_control(second_vehicle_control)
+
+        while True:
+            transform = ego_vehicle.get_transform()
+            spectator.set_transform(carla.Transform(transform.location + carla.Location(z=30),carla.Rotation(pitch=-90)))
+
+            if colision_detector(ego_vehicle, second_vehicle):
+                ego_vehicle_control.throttle = 0.0
+                ego_vehicle_control.brake = 1.0
+                ego_vehicle.apply_control(ego_vehicle_control)
+                print("Collision is about to occur !!!!!!")
+                break
+
+    finally:
+        input("Press Enter to continue")
+        vehicles_list = []
+        for vehicle in world.get_actors().filter('vehicle.*'):
+                if vehicle.attributes['role_name'] != 'hero': 
+                    vehicles_list.append(vehicle)
+        client.apply_batch([carla.command.DestroyActor(x) for x in vehicles_list])
         
 if __name__ == '__main__':
     try:
